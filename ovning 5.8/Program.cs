61ad95769c55940d2be679b9f7e95fe714f2510d
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 13:59:52 +0200

    Add project files.

---
+++ ovning 5.8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace övning_5._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] talArray = { 12345, 67890, 54321, 98765, 24680 };
+
+            foreach (int tal in talArray)
+            {
+                string talSträng = tal.ToString();
+
+                for (int i = 0; i < talSträng.Length; i++)
+                {
+                    Console.WriteLine(talSträng[i]);
+                    Thread.Sleep(500); // Kort paus mellan varje siffra
+                }
+
+                Thread.Sleep(2000); // Lite längre paus mellan varje tal
+            }
+        }
+    }
+}
